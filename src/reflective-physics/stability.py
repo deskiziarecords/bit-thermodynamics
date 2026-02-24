7009fc81f419c5f8e992a908412064c699cd2c00
J Roberto Jimenez <tijuanapaint@gmail.com>
2026-02-24 12:22:34 -0800

    Create stability.py

---
+++ src/reflective-physics/stability.py
@@ -0,0 +1,6 @@
+# stability.py
+class ReflectiveStability:
+    """Verifies K(psi)=0 and inconsistency_prob <= theta [8]"""
+    def check_fixed_point(self, field_state):
+        """If the system stops proving itself, it disappears [1]"""
+        pass
